8f069d7dff1ce403d375794f2daf4bc644b2744e
biscuolai <ilson_biscuola@yahoo.com>
2018-08-18 11:50:51 +0800

    installed flex layout and fixed login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,20 @@ import { AuthGuard } from './guards/auth.guard';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout'
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatExpansionModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule, MatProgressSpinnerModule, MatFormFieldModule, MatRippleModule, MatNativeDateModule, MatGridListModule, MatCardModule, MatMenuModule, MatRadioModule } from '@angular/material';
+import { MainNavigationComponent } from './main-navigation/main-navigation.component';
+import { AuthService } from './auth/auth.service';
+import { AppRoutingModule } from './app.routing.module';
+
+import { MatExpansionModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule, MatProgressSpinnerModule, MatFormFieldModule, MatRippleModule, MatNativeDateModule, MatGridListModule, MatCardModule, MatMenuModule, MatRadioModule, MatTabsModule } from '@angular/material';
 import { MatInputModule } from '@angular/material/input'
 import { MatSelectModule } from '@angular/material/select'
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { AppRoutingModule } from './app.routing.module';
-import { MainNavigationComponent } from './main-navigation/main-navigation.component';
-import { AuthService } from './auth/auth.service';
 
 @NgModule({
   declarations: [
@@ -29,6 +31,7 @@ import { AuthService } from './auth/auth.service';
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
+    FlexLayoutModule,
 
     AppRoutingModule,
 
@@ -53,7 +56,7 @@ import { AuthService } from './auth/auth.service';
     MatMenuModule,
     MatRadioModule,
     MatExpansionModule,
-
+    MatTabsModule
   ],
   providers: [
     AuthService,
